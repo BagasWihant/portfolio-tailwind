599d7b148539639138ef68e109f61cd099f866b8
bagaswihant <bagaswihantoro@rekadigi.id>
2023-08-12 20:27:20 +0700

    fix buttom darkmode

--- resources/views/livewire/component/navbar-component.blade.php
+++ resources/views/livewire/component/navbar-component.blade.php
@@ -1,21 +1,8 @@
 <div id='navbar'
-    class="fixed w-full h-20  z-[100] bg-[#ecf0f3] dark:bg-gray-800 dark:text-white transition-colors duration-300">
+    class="fixed w-full h-20  z-[100] bg-[#ecf0f3] text-gray-950 dark:bg-gray-800 dark:text-white transition-colors duration-300">
     <div class="flex justify-between items-center w-full h-full px-2 2xl:px-16">
         <div class="flex ">
             <img src="{{ asset('img/logo.png') }}" class="w-14 mx-2">
-            <button wire:ignore id="theme-toggle"
-                class="dark:text-gray-200 hover:bg-gray-200 dark:hover:bg-gray-700 focus:outline-none rounded-lg text-sm p-2.5">
-                <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5 text-blue" fill="currentColor" viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-                </svg>
-                <svg id="theme-toggle-light-icon" class="hidden w-5 h-5 " fill="currentColor" viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                        fill-rule="evenodd" clip-rule="evenodd"></path>
-                </svg>
-            </button>
         </div>
 
 
@@ -53,19 +40,6 @@
             <div class="flex justify-between items-center w-full">
                 <div class="flex ">
                     <img src="{{ asset('img/logo.png') }}" class="w-14 mx-2">
-                    <button wire:ignore id="theme-toggle2"
-                        class="dark:text-gray-200  hover:bg-gray-200 dark:hover:bg-gray-700 focus:outline-none rounded-lg text-sm p-2.5">
-                        <svg id="theme-toggle-dark-icon2" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-                        </svg>
-                        <svg id="theme-toggle-light-icon2" class="hidden w-5 h-5 " fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                                fill-rule="evenodd" clip-rule="evenodd"></path>
-                        </svg>
-                    </button>
                 </div>
                 <div class="rounded-full shadow-lg shadow-gray-400 p-1 cursor-pointer" wire:click='navOff'>
                     <x-antdesign-close-circle-o class="w-7" />
@@ -178,47 +152,5 @@
 
         });
 
-
-
-        var themeToggleBtn2 = document.getElementById('theme-toggle2');
-
-        var themeToggleDarkIcon2 = document.getElementById('theme-toggle-dark-icon2');
-        var themeToggleLightIcon2 = document.getElementById('theme-toggle-light-icon2');
-
-        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia(
-                '(prefers-color-scheme: dark)').matches)) {
-            themeToggleLightIcon2.classList.remove('hidden');
-        } else {
-            themeToggleDarkIcon2.classList.remove('hidden');
-        }
-
-        themeToggleBtn2.addEventListener('click', function() {
-
-            // toggle icons inside button
-            themeToggleDarkIcon2.classList.toggle('hidden');
-            themeToggleLightIcon2.classList.toggle('hidden');
-
-            // if set via local storage previously
-            if (localStorage.getItem('color-theme')) {
-                if (localStorage.getItem('color-theme') === 'light') {
-                    document.documentElement.classList.add('dark');
-                    localStorage.setItem('color-theme', 'dark');
-                } else {
-                    document.documentElement.classList.remove('dark');
-                    localStorage.setItem('color-theme', 'light');
-                }
-
-                // if NOT set via local storage previously
-            } else {
-                if (document.documentElement.classList.contains('dark')) {
-                    document.documentElement.classList.remove('dark');
-                    localStorage.setItem('color-theme', 'light');
-                } else {
-                    document.documentElement.classList.add('dark');
-                    localStorage.setItem('color-theme', 'dark');
-                }
-            }
-
-        });
     </script>
 @endpush
