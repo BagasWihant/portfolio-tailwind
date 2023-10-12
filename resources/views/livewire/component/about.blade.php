740478b191fd7d0752a6523f8194eb28f7ac4a27
bagaswihant <bagaswihant@gmail.com>
2023-10-12 10:25:41 +0700

    ganti liwewire ke js

--- resources/views/livewire/component/about.blade.php
+++ resources/views/livewire/component/about.blade.php
@@ -1,23 +1,60 @@
-<div id='about' class="w-full md:h-screen p-2 flex items-center py-16 dark:bg-gray-800 dark:text-white transition-colors duration-300">
+<div id='about'
+    class="w-full md:h-screen p-2 flex items-center py-16 dark:bg-gray-800 dark:text-white transition-colors duration-300">
     <div class="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
-        <div class="col-span-2"  data-aos="fade-down">
-            <p class="uppercase text-2xl tracking-widest bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-blue-400">Tentang Saya</p>
-            
+        <div class="col-span-2" data-aos="fade-down">
+            <p
+                class="uppercase text-2xl tracking-widest bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-blue-400">
+                Tentang Saya</p>
+
             <p class="py-2 text-gray-600 dark:text-gray-300">
-                Saya Bagas Wihantoro, lahir di Boyolali, Jawa Tengah. Saya lulus di Amikom Surakarta pada tahun 2021 dengandan prodi yang saya ambil yaitu Manajemen Informatika
+                Saya Bagas Wihantoro, lahir di Boyolali, Jawa Tengah. Saya lulus di Amikom Surakarta pada tahun 2021
+                dengan prodi yang saya ambil yaitu Manajemen Informatika. Saya telah menjadi Fullstack Developer selama
+                hampir 2 tahun, dan telah
+                membuat beberapa project baik untuk pribadi atau pun untuk perusahaan.
             </p>
-            <p class="py-2 text-gray-600 dark:text-gray-300">
-                Seorang fullstack web developer yang memiliki pengalaman membangun website selama hampir 2 tahun.
-            </p>
-            <button class="rounded-[20px] bg-gradient-to-r from-purple-600 to-blue-700 p-2 mt-3">
-                <a class="py-2 text-gray-100 cursor-pointer uppercase font-bold flex gap-2 items-center" href="#project">Lihat project saya 
-                
-                <svg fill="#ffffff" height="20px" width="20px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 330 330" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path id="XMLID_2_" d="M165,0C74.019,0,0,74.019,0,165s74.019,165,165,165s165-74.019,165-165S255.981,0,165,0z M225.606,175.605 l-80,80.002C142.678,258.535,138.839,260,135,260s-7.678-1.464-10.606-4.394c-5.858-5.857-5.858-15.355,0-21.213l69.393-69.396 l-69.393-69.392c-5.858-5.857-5.858-15.355,0-21.213c5.857-5.858,15.355-5.858,21.213,0l80,79.998 c2.814,2.813,4.394,6.628,4.394,10.606C230,168.976,228.42,172.792,225.606,175.605z"></path> </g></svg>
+            {{-- <p class="py-2 text-gray-600 dark:text-gray-300">
+
+            </p> --}}
+
+            <button class="rounded-[25px] bg-gradient-to-r from-purple-600 to-blue-700 p-2 mt-3 mr-3">
+                <a class="py-2 text-gray-100 cursor-pointer uppercase font-bold flex gap-2 items-center" target="_blank"
+                    href="https://drive.google.com/file/d/1EO6a4knmZyg3FHCqEPut9Q_3KeJs4e0E/view?usp=drive_link">Download
+                    CV
+
+                    <svg viewBox="0 0 24 24" fill="none" height='20px' width='20px' xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                            <path
+                                d="M17 9.00195C19.175 9.01406 20.3529 9.11051 21.1213 9.8789C22 10.7576 22 12.1718 22 15.0002V16.0002C22 18.8286 22 20.2429 21.1213 21.1215C20.2426 22.0002 18.8284 22.0002 16 22.0002H8C5.17157 22.0002 3.75736 22.0002 2.87868 21.1215C2 20.2429 2 18.8286 2 16.0002L2 15.0002C2 12.1718 2 10.7576 2.87868 9.87889C3.64706 9.11051 4.82497 9.01406 7 9.00195"
+                                stroke="#fff" stroke-width="1.5" stroke-linecap="round"></path>
+                            <path d="M12 2L12 15M12 15L9 11.5M12 15L15 11.5" stroke="#fff" stroke-width="1.5"
+                                stroke-linecap="round" stroke-linejoin="round"></path>
+                        </g>
+                    </svg>
+                </a>
+            </button>
+
+            <button class="rounded-[25px] bg-gradient-to-r from-purple-600 to-blue-700 p-2 mt-3 mr-3">
+                <a class="py-2 text-gray-100 cursor-pointer uppercase font-bold flex gap-2 items-center"
+                    href="#project">Lihat project
+
+                    <svg fill="#ffffff" height="20px" width="20px" version="1.1" id="Layer_1"
+                        xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                        viewBox="0 0 330 330" xml:space="preserve">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                            <path id="XMLID_2_"
+                                d="M165,0C74.019,0,0,74.019,0,165s74.019,165,165,165s165-74.019,165-165S255.981,0,165,0z M225.606,175.605 l-80,80.002C142.678,258.535,138.839,260,135,260s-7.678-1.464-10.606-4.394c-5.858-5.857-5.858-15.355,0-21.213l69.393-69.396 l-69.393-69.392c-5.858-5.857-5.858-15.355,0-21.213c5.857-5.858,15.355-5.858,21.213,0l80,79.998 c2.814,2.813,4.394,6.628,4.394,10.606C230,168.976,228.42,172.792,225.606,175.605z">
+                            </path>
+                        </g>
+                    </svg>
                 </a>
             </button>
         </div>
-        <div  data-aos="zoom-in-right" data-aos-easing="linear"
-     data-aos-duration="1500" class="w-full h-auto m-auto shadow-xl shadow-gray-300 rounded-xl items-center justify-center flex p-5 hover:scale-105 ease-in-out duration-300">
+        <div data-aos="zoom-in-right" data-aos-easing="linear" data-aos-duration="1000"
+            class="w-full h-auto m-auto shadow-xl shadow-gray-300 rounded-xl items-center justify-center flex p-5 hover:scale-105 ease-in-out duration-300">
             <img src="{{ asset('img/1.jpg') }}" class="rounded-xl">
         </div>
     </div>
--- resources/views/livewire/component/navbar-component.blade.php
+++ resources/views/livewire/component/navbar-component.blade.php
@@ -1,74 +1,78 @@
 <div class="flex justify-center sticky top-3 z-40">
-<div id='navbar'
-    class="w-[95%] h-20 z-[100] backdrop-blur-lg transition-colors duration-300 dark:text-white text-gray-950 rounded-xl">
-    <div class="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-        <div class="flex ">
-            <div class="w-14 mx-2 rounded-lg bg-image-dark dark:bg-image-light"></div>
-        </div>
-
-        <div>
-            <ul class="hidden md:flex">
-                <a href="#home">
-                    <li class="ml-10 uppercase font-bold hover:border-b ">Home</li>
-                </a>
-                <a href="#about">
-                    <li class="ml-10 uppercase font-bold hover:border-b ">About</li>
-                </a>
-                <a href="#skill">
-                    <li class="ml-10 uppercase font-bold hover:border-b">skill</li>
-                </a>
-                <a href="#project">
-                    <li class="ml-10 uppercase font-bold hover:border-b">project</li>
-                </a>
-                <a href="#contact">
-                    <li class="ml-10 uppercase font-bold hover:border-b">contact</li>
-                </a>
-            </ul>
-
-            @if(!$nav)
-            <div class="md:hidden" wire:click='navOn'>
-                <x-antdesign-menu-o class="w-10" />
+    <div id='navbar'
+        class="w-[95%] h-20 z-[100] backdrop-blur-lg transition-colors duration-300 dark:text-white text-gray-950 rounded-xl">
+        <div class="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+            <div class="flex ">
+                <div class="w-14 mx-2 ">
+                    <img src="{{ asset('img/logo.png') }}"  class="hidden rounded-lg" id="logo-light">
+                    <img src="{{ asset('img/dark.jpeg') }}" class="hidden rounded-lg"  id="logo-dark">
+                </div>
             </div>
-            @else
-            <div class="md:hidden" wire:click='navOff'>
-                <x-antdesign-close-circle-o class="w-10" />
+
+            <div>
+                <ul class="hidden md:flex">
+                    <a href="#home">
+                        <li class="ml-10 uppercase font-bold hover:border-b ">Home</li>
+                    </a>
+                    <a href="#about">
+                        <li class="ml-10 uppercase font-bold hover:border-b ">About</li>
+                    </a>
+                    <a href="#skill">
+                        <li class="ml-10 uppercase font-bold hover:border-b">skill</li>
+                    </a>
+                    <a href="#project">
+                        <li class="ml-10 uppercase font-bold hover:border-b">project</li>
+                    </a>
+                    <a href="#contact">
+                        <li class="ml-10 uppercase font-bold hover:border-b">contact</li>
+                    </a>
+                </ul>
+
+                <div class="md:hidden" id='btnopen' onclick="navOn()">
+                    <x-antdesign-menu-o class="w-10" />
+                </div>
+
+                <div class="md:hidden" id='btnclose' onclick='navOff()'>
+                    <x-antdesign-close-circle-o class="w-10" />
+                </div>
+
             </div>
-            @endif
+
         </div>
-
     </div>
-</div>
-<div
-    class="md:hidden p-4 fixed top-0 ease-in duration-700 w-full h-full backdrop-blur-3xl dark:bg-white/5 bg-white/25 dark:text-white text-gray-950 {{ $nav ? ' navopen' : ' navclose ' }}">
+    <div id='navbarDiv'
+        class="md:hidden p-4 fixed top-0 ease-in duration-700 w-full h-full backdrop-blur-3xl dark:bg-white/5 bg-white/25 dark:text-white text-gray-950">
 
-    <div class="flex flex-col justify-between h-full">
-    <div class="px-10 pt-20 flex flex-col ">
-        <ul class="uppercase" wire:click='navOff'>
-            <a href="#home">
-                <li class="py-4 font-bold ">Home</li>
-            </a>
-            <a href="#about">
-                <li class="py-4 font-bold">About</li>
-            </a>
-            <a href="#skill">
-                <li class="py-4 font-bold">skill</li>
-            </a>
-            <a href="#project">
-                <li class="py-4 font-bold">project</li>
-            </a>
-            <a href="#contact">
-                <li class="py-4 font-bold">contact</li>
-            </a>
-        </ul>
+        <div class="flex flex-col justify-between h-full">
+            <div class="px-10 pt-20 flex flex-col ">
+                <ul class="uppercase" onclick='navOff()'>
+                    <a href="#home">
+                        <li class="py-4 font-bold ">Home</li>
+                    </a>
+                    <a href="#about">
+                        <li class="py-4 font-bold">About</li>
+                    </a>
+                    <a href="#skill">
+                        <li class="py-4 font-bold">skill</li>
+                    </a>
+                    <a href="#project">
+                        <li class="py-4 font-bold">project</li>
+                    </a>
+                    <a href="#contact">
+                        <li class="py-4 font-bold">contact</li>
+                    </a>
+                </ul>
 
-    </div>
-    <div class="">
-        <p class="uppercase tracking-widest font-extrabold bg-clip-text bg-gradient-to-r dark:text-white text-gray-950">Lets connect</p>
-        @include('livewire.component.sosmed')
+            </div>
+            <div class="">
+                <p
+                    class="uppercase tracking-widest font-extrabold bg-clip-text bg-gradient-to-r dark:text-white text-gray-950">
+                    Lets connect</p>
+                @include('livewire.component.sosmed')
 
+            </div>
+        </div>
     </div>
-    </div>
-</div>
 </div>
 
 @push('scripts')
@@ -82,6 +86,45 @@
             }
         })
 
+        let statusNavbar = false;
+
+        navbarDiv = document.getElementById('navbarDiv')
+        btnopen = document.getElementById('btnopen')
+        btnclose = document.getElementById('btnclose')
+
+        document.addEventListener("DOMContentLoaded", () => {
+            cek()
+        });
+
+        function cek() {
+            console.log('navbar ' + statusNavbar);
+
+            if (statusNavbar) {
+                navbarDiv.classList.add('navopen')
+                navbarDiv.classList.remove('navclose')
+
+                btnclose.classList.remove('hidden')
+                btnopen.classList.add('hidden')
+            } else {
+                btnopen.classList.remove('hidden')
+                btnclose.classList.add('hidden')
+
+                navbarDiv.classList.remove('navopen')
+                navbarDiv.classList.add('navclose')
+            }
+        }
+
+        function navOn() {
+            statusNavbar = true;
+            cek()
+
+        }
+
+        function navOff() {
+            statusNavbar = false;
+            cek()
+        }
+
         if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia(
                 '(prefers-color-scheme: dark)').matches)) {
             document.documentElement.classList.add('dark');
@@ -91,13 +134,18 @@
 
         var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
         var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+        var logodark = document.getElementById('logo-dark');
+        var logolight = document.getElementById('logo-light');
 
         // Change the icons inside the button based on previous settings
         if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia(
                 '(prefers-color-scheme: dark)').matches)) {
             themeToggleLightIcon.classList.remove('hidden');
+            logolight.classList.remove('hidden');
         } else {
             themeToggleDarkIcon.classList.remove('hidden');
+            logodark.classList.remove('hidden');
+
         }
 
         var themeToggleBtn = document.getElementById('theme-toggle');
@@ -106,6 +154,8 @@
             // toggle icons inside button
             themeToggleDarkIcon.classList.toggle('hidden');
             themeToggleLightIcon.classList.toggle('hidden');
+            logodark.classList.toggle('hidden');
+            logolight.classList.toggle('hidden');
 
             // if set via local storage previously
             if (localStorage.getItem('color-theme')) {
@@ -129,6 +179,5 @@
             }
 
         });
-
     </script>
 @endpush
